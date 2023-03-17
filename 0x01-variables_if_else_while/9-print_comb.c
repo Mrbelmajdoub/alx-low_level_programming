0c483cf76051caa9bff305e9aed31adc6d9c9e77
Mrbelmajdoub <belmajdoub.saad@outlook.com>
2023-03-17 09:15:33 -0700

    1.9.0

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,9 +10,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
-		printf("%d", i);
-	printf("\n");
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
 
 	return (0);
 }
