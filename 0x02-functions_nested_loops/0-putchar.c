ec227c7dbec9cfd5765f425713add0f7fcc3477d
Mrbelmajdoub <belmajdoub.saad@outlook.com>
2023-03-21 08:47:54 -0700

    2.0.1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include<stdio.h>
+#include"main.h"
 
 /**
-  * main = Entery point
-  *
+  * main - check desctiption
+  * description: it prints the word _putchar, followed by a new line
   * Return: Always 0 (Success)
   */
 int main(void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
