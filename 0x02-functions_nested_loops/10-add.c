7f95bc60622c5d58ff5873ccb03ec1febed938bd
Mrbelmajdoub <belmajdoub.saad@outlook.com>
2023-03-21 14:25:11 -0700

    2.10.3

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-  * add - adds twa integers and returns results
-  * @n: first number to be added
-  * @m: second number to be added
-  * Ruturn: Always 0.
+  * add - print adds twa integers and returns results
+  * @n: first number to be added int n
+  * @m: second number to be added int m
+  * Return: Always 0.
   */
 int add(int n, int m)
 {
