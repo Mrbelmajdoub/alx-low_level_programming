b72ad70660a0872125c10fc46d757af49052af73
Mrbelmajdoub <belmajdoub.saad@outlook.com>
2023-03-21 14:45:22 -0700

    copied, additional tasks for point, and i am dead tired today,so revise this tomorrow

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H_INCLUDED
+
+#define MAIN_H_INCLUDED
+
+
+
 int _putchar(char c);
 
 void print_alphabet(void);
@@ -16,8 +22,14 @@ int print_last_digit(int n);
 
 void jack_bauer(void);
 
-void times_table();
+void times_table(void);
 
-int add(int n, int m);
+int add(int a, int b);
 
 void print_to_98(int n);
+
+void print_times_table(int n);
+
+
+
+#endif
