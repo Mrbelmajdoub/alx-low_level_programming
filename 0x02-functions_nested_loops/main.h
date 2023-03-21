b592328ddc4da10bafae75a7a40bc977ac761383
Mrbelmajdoub <belmajdoub.saad@outlook.com>
2023-03-21 13:28:58 -0700

    2.9.0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H_INCLUDED
+
+#define MAIN_H_INCLUDED
+
+
+
 int _putchar(char c);
 
 void print_alphabet(void);
@@ -12,6 +18,18 @@ int print_sign(int n);
 
 int _abs(int n);
 
-int print_last_digit(int);
+int print_last_digit(int n);
 
 void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+
+
+#endif
