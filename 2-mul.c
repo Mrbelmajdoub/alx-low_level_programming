c6e556d4864a04d03b64f242b340bdd603961b4c
Mrbelmajdoub <belmajdoub.saad@outlook.com>
2023-03-22 22:35:25 -0700

    4.3.0

---
+++ 2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * mul - multiplies two integers
+  * @a: first int
+  * @b: second int
+  * Return: the result of multiplication r
+  */
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+	return (r);
+}
