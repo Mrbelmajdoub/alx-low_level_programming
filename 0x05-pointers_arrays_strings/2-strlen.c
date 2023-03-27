89d41bca12a541f92a3d88d783525d5ee51b3174
Mrbelmajdoub <belmajdoub.saad@outlook.com>
2023-03-27 16:53:18 +0000

    5.2.0

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+  * _strlen - return the length of a string
+  * @s: the string
+  * return: the length of the string
+  */
+int _strlen(char *s)
+{
+	return (sizeof(s));
+}
