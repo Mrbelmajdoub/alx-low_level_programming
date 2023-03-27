70724c8d6ded47f688f71f7a1d1b23bd2817f018
Mrbelmajdoub <belmajdoub.saad@outlook.com>
2023-03-27 16:59:10 +0000

    5.2.1

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,15 @@
 /**
   * _strlen - return the length of a string
   * @s: the string
-  * return: the length of the string
+  * Return: the length of the string
   */
 int _strlen(char *s)
 {
-	return (sizeof(s));
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
